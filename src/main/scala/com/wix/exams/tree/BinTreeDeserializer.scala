17495d9e9b9160937cdd8b2363761952380a5c6c
Cyril Ponomaryov <cyril.ponomaryov@gmail.com>
2017-07-27 18:04:49 +0300

    BinTreeDeserializer scanning implementation

--- src/main/scala/com/wix/exams/tree/BinTreeDeserializer.scala
+++ src/main/scala/com/wix/exams/tree/BinTreeDeserializer.scala
@@ -11,13 +11,28 @@ object BinTreeDeserializer {
   }
 
   private def nextToken(reader: Reader): Option[Token] = {
-    val ch = reader.read()
-    ch match {
+    reader.read().toChar match {
+      case -1 => None
       case 'n' => Some(Null)
-      case 'v' => {
-
-        val length = reader.readInt()
-      }
+      case 'v' =>
+        val lengthStr = new StringBuilder
+        var ch = 'a'
+        while (true) {
+          ch = reader.read().toChar
+          if (ch == ':') {
+            val length = lengthStr.toInt
+            val cbuf = new Array[Char](length)
+            reader.read(cbuf)
+            return Some(Value(String.valueOf(cbuf)))
+          } else if (Character.isDigit(ch)) {
+            lengthStr.append(ch)
+          } else {
+            throw new ScanningException(s"Unexpected char: $ch, possible chars: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']")
+          }
+        }
+        throw new ScanningException
+      case 'e' => Some(End)
+      case ch => throw new ScanningException(s"Unexpected char: $ch, possible chars: ['n', 'v', 'e']")
     }
   }
 
@@ -25,4 +40,6 @@ object BinTreeDeserializer {
   private case object Null extends Token
   private case class Value(value: String) extends Token
   private case object End extends Token
+
+  class ScanningException(msg: String = "Scanning failed", cause: Throwable = null) extends RuntimeException(msg, cause)
 }
