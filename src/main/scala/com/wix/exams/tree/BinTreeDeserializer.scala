3f3497f0b47e856b065d4308b64d1e8c7d595cc7
Cyril Ponomaryov <cyril.ponomaryov@gmail.com>
2017-07-27 17:32:59 +0300

    BinTreeDeserializer implementation started

--- src/main/scala/com/wix/exams/tree/BinTreeDeserializer.scala
+++ src/main/scala/com/wix/exams/tree/BinTreeDeserializer.scala
@@ -1,8 +1,28 @@
 package com.wix.exams.tree
 
+import java.io.{Reader, StringReader}
+
 object BinTreeDeserializer {
 
   def apply(src: String): BinTree = {
+    val reader = new StringReader(src)
+
 
   }
+
+  private def nextToken(reader: Reader): Option[Token] = {
+    val ch = reader.read()
+    ch match {
+      case 'n' => Some(Null)
+      case 'v' => {
+
+        val length = reader.readInt()
+      }
+    }
+  }
+
+  private sealed trait Token
+  private case object Null extends Token
+  private case class Value(value: String) extends Token
+  private case object End extends Token
 }
